6c311f7910bb765bc64604eb550da94950c971e0
kherry07 <159637026+kherry07@users.noreply.github.com>
2025-07-06 16:16:33 -0600

    Add files via upload

---
+++ reverse_flash.java
@@ -0,0 +1,15 @@
+package Detroit_Become_Human;
+
+public class reverse_flash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int i;
+		
+		for ( i=1;i<=100;i++)
+			if (i % 2 ==0 && i % 3 ==0 ) {
+				System.out.println(i);
+			}
+	}
+
+}
